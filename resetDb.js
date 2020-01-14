fda3cd395a27f089a011736242b8bdb4ab3b624b
Maleeha <malehahasan@gmail.com>
2020-01-14 13:14:18 -0500

    [resetDb]+[seed] initialised db

---
+++ resetDb.js
@@ -0,0 +1,14 @@
+const { sequelize } = require('./models');
+const main = async () => {
+  try {
+    await sequelize.sync({ force: true })
+  }
+  catch (e) {
+    console.error(e.message);
+  } finally {
+    process.exit();
+  }
+}
+
+
+main();
